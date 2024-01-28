dd41ee6518deb010bbbcc0bb6eff3fccadc1413b
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2024-01-28 08:56:50 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -6,7 +6,6 @@ if (year%100==0 and year%400==0) or (year%4==0 and year%100 !=0):
 else:
     print("It is not a leap year.")
    # ------------------------------------
-
     ##4 se h to 100 se ni hona chiye
     ###4,400,100 teeno se ho to leap yaer
 
